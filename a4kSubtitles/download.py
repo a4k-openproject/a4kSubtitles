a4743bd8c0cb9b6d0d426f63d3fc6dcccfcd1c00
newt-sc <47229722+newt-sc@users.noreply.github.com>
2020-04-23 00:35:40 +0300

    release: v0.0.12

--- a4kSubtitles/download.py
+++ a4kSubtitles/download.py
@@ -6,29 +6,39 @@ def __download(core, filepath, request):
         with open(filepath, 'wb') as f:
             core.shutil.copyfileobj(r.raw, f)
 
-def __extract(core, archivepath, filename, language):
+def __extract(core, archivepath, filename, lang):
     path = core.utils.quote_plus(archivepath)
     ext = core.os.path.splitext(filename)[1].lower()
     (dirs, files) = core.kodi.xbmcvfs.listdir('archive://%s' % path)
 
     subfile = filename
     for file in files:
+        if core.utils.PY2:
+            file = file.decode('utf8')
         if file.lower().endswith(ext):
             subfile = file
             break
 
     src = 'archive://' + path + '/' + subfile
-    subfile = __insert_language_code_in_filename(core, filename, language)
+    subfile = __insert_lang_code_in_filename(core, filename, lang)
     dest = core.os.path.join(core.utils.temp_dir, subfile)
     core.kodi.xbmcvfs.copy(src, dest)
     return dest
 
-def __insert_language_code_in_filename(core, filename, language_name):
+def __insert_lang_code_in_filename(core, filename, lang):
     filename_chunks = filename.split('.')
-    lang_code = core.kodi.xbmc.convertLanguage(language_name,
-                                               core.kodi.xbmc.ISO_639_1)
+    lang_code = core.kodi.xbmc.convertLanguage(lang, core.kodi.xbmc.ISO_639_1)
     filename_chunks.insert(-1, lang_code)
     return '.'.join(filename_chunks)
+
+def __cleanup(core, filepath):
+    with open(filepath, 'r') as f:
+        sub_contents = f.read()
+
+    sub_contents = core.utils.cleanup_subtitles(sub_contents)
+
+    with open(filepath, 'w') as f:
+        f.write(sub_contents)
 
 def download(core, params):
     core.logger.debug(lambda: core.json.dumps(params, indent=2))
@@ -47,6 +57,9 @@ def download(core, params):
 
     __download(core, archivepath, request)
     filepath = __extract(core, archivepath, filename, language)
+
+    if core.kodi.get_bool_setting('general.remove_ads'):
+        __cleanup(core, filepath)
 
     if core.api_mode_enabled:
         return filepath
