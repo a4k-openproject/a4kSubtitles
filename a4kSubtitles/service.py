c039154fa1e74611bf25f138c38523d9b8c86407
peno64 <32430988+peno64@users.noreply.github.com>
2023-10-29 21:09:06 +0100

    Update service.py
    only download subtitles if there is an imdb number

--- a4kSubtitles/service.py
+++ a4kSubtitles/service.py
@@ -25,6 +25,15 @@ def start(api):
         has_done_subs_check = True
         has_subtitles = False
 
+        # Check if there is an IMDB number for this video
+        # If not then this is most probably not a video played from an addon that provides the IMDB number
+        # It could also be a local video file of from youtube, a tv station or...
+        # In that case don't set the subtitle stream
+        # Maybe an option should be added to the settings of a4kSubtitles if this is wanted or not
+        IMDBNumber = core.kodi.xbmc.getInfoLabel('VideoPlayer.IMDBNumber')
+        if IMDBNumber is None or IMDBNumber == '':
+            continue
+
         preferredlang = core.kodi.get_kodi_setting('locale.subtitlelanguage')
 
         try:
